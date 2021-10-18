4e760b003f2fd6f9c3484e382d6622567b5df028
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-18 19:36:03 +0530

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
