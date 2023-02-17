702894b72cee21baeef8a41d1f2630ddfb04bdc1
uvacoder <uva22940coder@gmail.com>
2023-02-17 10:15:41 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: [],
   mode: "jit",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
